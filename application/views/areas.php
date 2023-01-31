b26775a47157459d38b0f9291c8574b926d43e83
George Raveen <georgeraveen@gmail.com>
2023-01-31 18:55:54 +0530

    Update areas.php

--- application/views/areas.php
+++ application/views/areas.php
@@ -1,26 +1,57 @@
 
 <?php
 
-$towns = ['Negombo','Katana','Kochikade','Raddolugama','Katunayake','Seeduwa','Averiwatta','Bopitiya','Ja-Ela',
-		'Kapuwatte','Ekala','Pamunugama','Kandana','Enderamulla','Hunupitiya','Hendala','Ragama','Welisara',
-		'Elakande','Mabole','Mahabage','Wattala','Peliyagoda','Divulapitiya','Minuwangoda'];
-$areas = ['Negombo','Katana','Seeduwa','Peliyagoda'];
+$areas = ["SEEDUWA","JA - ELA",	"GAMPAHA","KANDANA","DANKOTUWA","WENNAPPUWA","MARAWILA","WATTALA","MIRIGAMA","NEGOMBO",	"KOCHCHIKADE","MINUWANGODA","DIVULAPITIYA",	"NATTANDIYA"];
 
 $areaTowns = array();
 foreach($areas as $area){
 	$areaTowns[$area] = array();
-	if($area == 'Negombo'){
-		$areaTowns[$area] = ['Negombo','Katana','Kochikade','Raddolugama','Katunayake'];
+	if($area == 'SEEDUWA'){
+		$areaTowns[$area] = ["Raddolugama",	"Averiwatta","Katunayake","Amandoluwa","Liyanagemulla","Kotugoda","Seeduwa"];
 	}
-	if($area == 'Katana'){
-		$areaTowns[$area] = ['Katana','Seeduwa','Averiwatta','Bopitiya','Ja-Ela'];
+	if($area == 'JA - ELA'){
+		$areaTowns[$area] = [ "Pamunugama","Bopitiya","Kapuwatta","Ekala","Niwasipura","Uswatakeiyawa","Makewita","Thudella","Ja-ela"];
 	}
-	if($area == 'Seeduwa'){
-		$areaTowns[$area] = ['Seeduwa','Kapuwatte','Ekala','Pamunugama','Kandana'];
+	if($area == 'GAMPAHA'){
+		$areaTowns[$area] = ["Udugampola","Gampaha"];
 	}
-	if($area == 'Peliyagoda'){
-		$areaTowns[$area] = ['Peliyagoda','Enderamulla','Hunupitiya','Hendala','Ragama'];
+	if($area == 'KANDANA'){
+		$areaTowns[$area] = ["Rilaulla","Nagoda","Ganemulla","Polpithimukalana","Kandana"];
 	}
+	if($area == 'DANKOTUWA'){
+		$areaTowns[$area] = ["Kottramulla","Kirimetiyana","Dankotuwa"];
+	}
+	if($area == 'WENNAPPUWA'){
+		$areaTowns[$area] = ["Nainamadama","Lunuwila","Waikkala","Bolawatta","Wenappuwa"];
+	}
+	if($area == 'MARAWILA'){
+		$areaTowns[$area] = ["Mudukatuwa","Katuneriya","Marawila"];
+	}
+	if($area == 'WATTALA'){
+		$areaTowns[$area] = ["Ragama","Elakanda","Enderamulla","Hunupitiya","Hendala","Mahabage","Mabola","Welisara","Peliyagoda","Delpe","Peralanda","Hekiththa","Wattala"];
+	}
+	if($area == 'MIRIGAMA'){
+		$areaTowns[$area] = ["Pallewela","Mirigama"];
+	}
+	if($area == 'NEGOMBO'){
+		$areaTowns[$area] = ["Katana","Badalgama","Thalahena","Pitipana","Munnakara","Kandawala","Kurana","Kadolkale","Periyamulla","Akaragama","Negombo"];
+	}
+	if($area == 'KOCHCHIKADE'){
+		$areaTowns[$area] = ["Kattuwa","Dalupotha","Kochchikade"];
+	}
+	if($area == 'MINUWANGODA'){
+		$areaTowns[$area] = ["Andiambalama","Dagonna","Kimbulapitiya","Minuwangoda"];
+	}
+	if($area == 'DIVULAPITIYA'){
+		$areaTowns[$area] = ["Marandahamula","Dunagaha","Divulapitiya"];
+	}
+	if($area == 'NATTANDIYA'){
+		$areaTowns[$area] = ["Nattandiya"];
+	}
+}
+$towns = array();
+foreach($areaTowns as $area => $townList){
+	$towns = array_merge($towns,$townList);
 }
 
 
